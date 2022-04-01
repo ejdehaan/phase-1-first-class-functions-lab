5c682fca59e0458f005bc03b54307eb8d20c482f
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-04-01 17:32:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (array) {
+    let newArray = array.slice(0, 2)
+    return newArray
+}
+
+const returnLastTwoDrivers = function (array) {
+    let newArray = array.slice(2, 5)
+    return newArray
+}
+
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+];
+   
+
+
+function fareMultiplier() {
+    return num1 * multiplier
+}
+
+function createFareMultiplier() {
+    return fareMultiplier()
+}
+
+
+
+
+
